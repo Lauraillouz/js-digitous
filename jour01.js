c1f4403bbf45cd98f476e8f109e39483b11b91c9
Laura Illouz <laura.hortense@gmail.com>
2021-06-14 13:49:34 +0200

    exercises 1 to 8

---
+++ jour01.js
@@ -0,0 +1,35 @@
+/* // 01 - Hello World
+console.log("Hello World!"); 
+
+// 02 - String
+var test = "My name is Laura";
+console.log(test); 
+
+// 03 - Concatenation
+var name = "Laura";
+console.log("Nice to meet you" + " " + name); 
+
+// 04 - String Length
+var testLength = "I'm very long!";
+console.log(testLength.length); 
+
+// 05 - Replace
+var food = "croissant is meh";
+var food = food.replace("meh", "so good");
+console.log(food); 
+
+// 06 Up and Down
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic, basicUp, basicDown); 
+
+// 07 - Split
+var word = "banana";
+var letters = word.split("");
+console.log(letters); 
+
+// 08 - Template
+var age = 32;
+var template = (`I'm ${age} years old`);
+console.log(template); */
