c8ccbe7c0bc6e2d6799ba0223b50270ef47bdc52
Laura Illouz <laura.hortense@gmail.com>
2021-06-21 17:12:38 +0200

    4 first exercises of hardLoop

--- hardLoop.js
+++ hardLoop.js
@@ -1,1 +1,50 @@
-// 01 - Sum
+/* // 01 - Sum
+let numbers = [];
+let min = 1;
+let max = 100;
+for (let i = 0; i < 50; i++) {
+    let randomNumber  = Math.floor(Math.random() * (max - min + 1) + min);
+    numbers.push(randomNumber);
+}
+let sumNumbers = 0;
+for (let i = 0; i <= numbers.length; i++) {
+    sumNumbers += numbers[i];
+}
+console.log(sumNumbers); 
+
+// 02 - Max
+let numbers = [];
+let min = 50;
+let max = 200;
+for (let i = 0; i < 50; i++) {
+    let randomNumber  = Math.floor(Math.random() * (max - min + 1) + min);
+    numbers.push(randomNumber);
+}
+let maxNumber = 0;
+for (let i = 0; i <= numbers.length; i++) {
+    if (maxNumber < numbers[i]) {
+        maxNumber = numbers[i];
+    }
+}
+console.log(maxNumber); 
+
+// 03 - Unique
+let numbers = [];
+let min = 50;
+let max = 200;
+for (let i = 0; i < 50; i++) {
+    let randomNumber  = Math.floor(Math.random() * (max - min + 1) + min);
+    numbers.push(randomNumber);
+}
+console.log(numbers);
+let myNumber = 0;
+let i = 0;
+while (i <= numbers.length) {
+    i++;
+    if (numbers[i] >= 75 && numbers[i] <= 100) {
+        myNumber = numbers[i];
+        break;
+    }
+}
+console.log(`${myNumber} is the first number between 75 and 100`); */
+
--- whileLoop.js
+++ whileLoop.js
@@ -21,34 +21,6 @@ while (min <= max) {
  console.log(count); 
 
  // 03 - Course
- let ussainBolt = 0;
- let tysonGay = 0;
- let stepMin = 0;
- let stepMax = 10;
- while (ussainBolt <= 100 && tysonGay <= 100) {
-    let stepUssainBolt = Math.floor(Math.random() * (stepMax - stepMin + 1) + stepMin);
-    let stepTysonGay = Math.floor(Math.random() * (stepMax - stepMin + 1) + stepMin);
-    ussainBolt += stepUssainBolt;
-    tysonGay += stepTysonGay;
- }
- if (ussainBolt >= 100) {
-    console.log(`Avec ${ussainBolt} mètres, Ussain Bolt a gagné !`);
-}
-else if (tysonGay >= 100) {
-    console.log(`Avec ${tysonGay} mètres, Tyson Gay a gagné !`);
-} 
-
-// 04 - Des boîtes
-5
-2
-5
--8 
-
-// 05 - Encore des boîtes
-// Le pièèèèèège !  i n'est pas strictement supérieur à 0. Donc la boucle ne part pas.
-console.log(i) // affichera donc 0
-console.log(box1) // affichera 7 :) */
-
 let ussainBolt = 0;
 let tysonGay = 0;
 let stepMin = 0;
@@ -67,3 +39,15 @@ else if (tysonGay > ussainBolt) {
 } else {
     console.log("Égalité parfaite");
 }
+
+// 04 - Des boîtes
+5
+2
+5
+-8 
+
+// 05 - Encore des boîtes
+// Le pièèèèèège !  i n'est pas strictement supérieur à 0. Donc la boucle ne part pas.
+console.log(i) // affichera donc 0
+console.log(box1) // affichera 7 :) */
+
