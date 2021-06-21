01283a1acf60c03887eba65099fa215845dbcd4a
Laura Illouz <laura.hortense@gmail.com>
2021-06-21 13:23:43 +0200

    Exercices 1 to 3

---
+++ forLoop.js
@@ -0,0 +1,27 @@
+/* // 01 - Somme des carrés
+let result = 0;
+for (let i = 5; i <=10 ; i++) {
+    result += i*i;
+}
+console.log(result); 
+
+// 02 - Comptons
+let result = 0;
+for (let i = 100; i <= 1000; i++) {
+    if (i % 7 === 0) {
+        result += 1 ;
+    }
+}
+console.log(result); 
+
+// 03 - Chanceux
+let result = 0;
+for (let i = 1; i <= 20; i++) {
+    let min = 1;
+    let max = 6;
+    let random =  Math.floor(Math.random() * (max - min + 1) + min);
+    if (random >= 5) {
+        result += random;
+    }
+}
+console.log(result); */
