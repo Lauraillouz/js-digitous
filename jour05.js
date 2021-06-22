c1d9bfd9dae041928a26302e462bd02e0ce277ac
Laura Illouz <laura.hortense@gmail.com>
2021-06-22 16:52:03 +0200

    Exercise 04

--- jour05.js
+++ jour05.js
@@ -23,7 +23,7 @@ console.log(calculated); */
 let numberOne = parseInt(stringNumbers[0]);
 let operator = stringNumbers[1];
 let numberTwo = parseInt(stringNumbers[2]);
-calculate(numberOne, operator, numberTwo); */
+calculate(numberOne, operator, numberTwo); 
 
 // 02 - Table
 var {multiply, addition} = require("./table-utils.js");
@@ -31,6 +31,41 @@ let stringNumber = process.argv.slice(2);
 let number = parseInt(stringNumber[0]);
 multiply(number); 
 
-// Separate Table
+// 03 - Separate Table
 addition(number);
+*/
 
+// 04 - Guess
+var prompt = require("prompt");
+
+prompt.start();
+
+let min = 1;
+let max = 100;
+let mysteryNum = Math.floor(Math.random() * (max - min) + min);
+console.log(mysteryNum);
+function play() {
+    prompt.get({
+        name: "num",
+        description: "Quel est le nombre mystère ?"
+    }, function (err, res) {
+        res.num = parseInt(res.num);
+        console.log(res.num);
+        if (typeof res.num !== "number") {
+            console.log("error");
+            play();
+        }
+        else if (res.num < mysteryNum) {
+            console.log("C'est plus !");
+            play();
+        }
+        else if (res.num > mysteryNum) {
+            console.log("C'est moins !");
+            play();
+        }
+        else if (res.num === mysteryNum) {
+            console.log("Bravo !!");
+        }
+    })
+}
+play();
