fe6d9f03386201c000c436499c227714c2d3f1a1
Laura Illouz <laura.hortense@gmail.com>
2021-06-23 21:19:56 +0200

    Bonus Motus is now at least making some sense, but still not working as required

--- jour05.js
+++ jour05.js
@@ -43,7 +43,6 @@ prompt.start();
 let min = 1;
 let max = 100;
 let mysteryNum = Math.floor(Math.random() * (max - min) + min);
-console.log(mysteryNum);
 
 function play() {
     prompt.get({
@@ -68,8 +67,8 @@ function play() {
         }
     })
 }
-play(); */
-
+play(); 
+*/
 
 
 // BONUS *
@@ -78,26 +77,44 @@ var colors = require("colors");
 
 prompt.start();
 
-let words = ["brute", "poule", "botte", "pelle", "maire", "tarte", "terre"]
-let randomWord = words[Math.floor(Math.random() * words.length)];
-var count = 0;
+var words = ["brute", "poule", "botte", "pelle", "maire", "tarte", "terre"]
+var randomWord = words[Math.floor(Math.random() * words.length)];
+var count = 1;
+var i;
+var j;
 function motus () {
     prompt.get({
         name: "word",
-        description: "Prêt à jouer à Motus ?"
+        description: "Quel est le mot mystère ? Dites 'go' pour commencer",
     }, function (err, res) {
-        if (count === 0) {
-            console.log(randomWord[0]);
+        let guess = "";
+        if (count === 1) {
+            console.log(`La première lettre est ${randomWord[0]}`);
         }
         else if (count > 6) {
-            return console.log("Perdu !!");
+            return console.log("dommage...");
+        } else if (count > 6 && guess.length > randomWord.length) {
+            return console.log("Trop de lettres, dommage...");
         }
-        for (let i = 0; i < randomWord.length; i++) {
-            if (res.word[i] === randomWord[i]) {
-                res.word[i] = res.word[i].red;
-                console.log(res.word);
+
+        if (count <= 6 && guess !== randomWord) {
+        for (i = 0; i < randomWord.length; i++) {
+            if (res.word === randomWord) {
+                console.log("Bravo tu as gagné !");
+                return;
             }
+            else if (res.word[i] === randomWord[i]) {
+                guess += res.word[i].red;
+                // res.word[i].replace(res.word[i].red);
+                console.log(guess);
+            }
+            /* else if (res.word[j] === randomWord[j] && j !== i) {
+                guess += res.word[j].yellow;
+                console.log(guess);
+            } */
         } 
+
+        }
         count++;
         motus();
     })
