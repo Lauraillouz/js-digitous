16a31e67d04b6f25ebcfde60ec7a8f667752cad5
Laura Illouz <laura.hortense@gmail.com>
2021-06-23 14:46:50 +0200

    Exercise 02

---
+++ Révisions/jour06.js
@@ -0,0 +1,30 @@
+/* // 01 - Alphabet
+function sortLetters (string) {
+    console.log(string.split("").sort().join(""));
+}
+
+sortLetters("konexio"); 
+
+// 02 - XOXO
+function countEach (string) {
+    let countX = 0;
+    let countO = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "x") {
+            countX++;
+        }
+        else if (string[i] === "o") {
+            countO++;
+        }
+    }
+    if (countX === countO) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
+countEach("xoxo"); */
+
+// 03 - Palindrome
+
+
