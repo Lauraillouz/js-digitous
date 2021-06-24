b1a2c546dc92381c1734ff3183a59bf6d4ae1982
Laura Illouz <laura.hortense@gmail.com>
2021-06-24 14:08:45 +0200

    Exercise 02

--- jour06.js
+++ jour06.js
@@ -1,8 +1,16 @@
-// O1 - Format
+/* // O1 - Format
 (function formatDate (string) {
     let myDate = new Date();
     console.log(`${myDate.getDate()}/0${myDate.getMonth() + 1}/${myDate.getFullYear()}`);
-})();
+})(); */
  
 
 // 02 - Age
+function calculateAge (string) {
+    let today = new Date();
+    let birthYear = string.substring(0, 4);
+    birthYear = parseInt(birthYear);
+    let age = today.getFullYear() - birthYear;
+    console.log(age);
+}
+calculateAge("1989-05-24");
