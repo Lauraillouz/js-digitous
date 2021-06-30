c728590710c8f6f7c857cbfb21bbafca4d813d54
Laura Illouz <laura.hortense@gmail.com>
2021-06-30 14:49:45 +0200

    function moveForward, and attempt pilotRover

--- projet.js
+++ projet.js
@@ -1,50 +1,52 @@
 let prompt = require("prompt")
 prompt.start()
+
 // Initialisation du rover
 let grid = [
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 let rover = {
-    direction: "N"
+    direction: "N",
+    x: 0,
+    y: 0
 }
 
-const turnLeft = () => {
-
-    switch(rover.direction){
+// Tourner le rover à gauche
+const turnLeft = (rover) => {
+    switch ((rover).direction) {
         case "N":
-            rover.direction = "W";
+            (rover).direction = "W";
             break;
         case "W":
-            rover.direction = "S";
+            (rover).direction = "S";
             break;
         case "S":
+            (rover).direction = "E";
+            break;
+        case "E":
+            (rover).direction = "N";
+            break;
+    }
+    console.log((rover).direction)
+}
+
+// Tourner le rover à droite
+const turnRight = (rover) => {
+    console.log("Je teste la fonction turnRight", rover)
+    switch (rover.direction) {
+        case "N":
             rover.direction = "E";
             break;
         case "E":
-            rover.direction = "N";
-            break;
-    }
-
-}
-turnLeft();
-console.log(rover.direction)
-
-const turnRight = () => {
-
-    switch(rover.direction){
-        case "N":
-            rover.direction = "E";
-            break;
-        case "E":
             rover.direction = "S";
             break;
         case "S":
@@ -54,7 +56,53 @@ const turnRight = () => {
             rover.direction = "N";
             break;
     }
-
+    console.log(rover.direction)
 }
-turnRight();
-console.log(rover.direction)
+
+
+const moveForward = (rover) => {
+
+    switch (rover.direction) {
+        case "N":
+            rover.y--;
+            break;
+        case "E":
+            rover.x++;
+            break;
+        case "S":
+            rover.y++;
+            break;
+        case "W":
+            rover.x--;
+            break;
+    }
+}
+
+// Placement inital du rover
+let roverPosition = grid[(rover.y)][rover.x] = rover.direction;
+if (rover.x === 0 && rover.y === 0) {
+    roverPosition;
+}
+
+/* console.log(rover);
+turnLeft(rover);
+moveForward(rover);
+moveForward(rover);
+turnRight(rover);
+moveForward(rover); */
+
+// console.log(rover);
+
+const pilotRover = (str) => {
+
+    if (str === "l") {
+        turnLeft(rover);
+    } else if (str === "r") {
+        turnRight(rover)
+    } else if (str === "f") {
+        moveForward(rover)
+    }
+}
+
+pilotRover("r")
+console.table(grid);
